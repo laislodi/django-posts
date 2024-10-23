4aa5c3d7037625e5af3553b406cfb922ef8325ac
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2024-10-23 17:20:47 -0600

    Attempt to fix the deployment

--- django_posts/settings.py
+++ django_posts/settings.py
@@ -27,7 +27,11 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '*',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Without this, the frontend doesn't work
 # TODO: Investigate why
@@ -135,7 +139,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
